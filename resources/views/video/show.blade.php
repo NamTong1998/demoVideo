b25add7d1b4f55ba707a9c6cabec62cc1ae04346
NamTong1998 <nam.th.vnist@gmail.com>
2019-04-30 16:12:39 +0700

    adjust media player area

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -6,6 +6,6 @@
 <body>
 	<h2> <a href="{{ route('list') }}"> Go Back </a> </h2>
 	<h1> {{ $video->title }} </h1>
-	<video src="{{ asset('/storage/'.$video->path) }}" autobuffer autoplay="true" autoloop loop controls poster="/images/video.png"> Format Unsupported </video>
+	<video style="width: 900px; height: auto;" src="{{ asset('/storage/'.$video->path) }}" autobuffer autoplay="true" autoloop loop controls poster="/images/video.png"> Format Unsupported </video>
 </body>
 </html>
